ca3ba68233f03250a6ab6515fed896a0d6fc108d
Phillipml <contato.phillip.menezes@gmail.com>
2023-06-23 17:35:51 -0300

    #03 routes to reg/login/getUser/delete are added

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const swagger = require("./swagger");
 
 const app = express();
+app.use(express.json());
+swagger(app);
 const port = process.env.PORT || 3001;
 const dbConnect = process.env.DB_URL;
 const jwtSecret = process.env.JWT_SECRET;
@@ -26,21 +29,67 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
+/**
+ * @openapi
+ * /api/register:
+ *   post:
+ *     summary: Register a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ *       400:
+ *         description: Email already registered
+ */
 app.post("/api/register", async (req, res) => {
   const { email, username, password } = req.body;
 
   const existingUser = await User.findOne({ email });
   if (existingUser) {
-    return res.status(400).json({ error: "Email Already Registered" });
+    return res.status(400).json({ error: "Email already registered" });
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = new User({ email, username, password: hashedPassword });
   await newUser.save();
 
-  res.status(201).json({ message: "User Created Successfully" });
+  res.status(201).json({ message: "User created successfully" });
 });
 
+/**
+ * @openapi
+ * /api/login:
+ *   post:
+ *     summary: Login and get an access token
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Successful login
+ *       401:
+ *         description: Invalid email or password
+ */
 app.post("/api/login", async (req, res) => {
   const { email, password } = req.body;
 
@@ -53,29 +102,92 @@ app.post("/api/login", async (req, res) => {
     return res.status(401).json({ error: "Invalid email or password" });
   }
 
-  app.get("/api/user", async (req, res) => {
-    const token = req.headers.authorization;
-
-    if (!token) {
-      return res.status(401).json({ error: "No token provided" });
-    }
-
-    try {
-      const decoded = jwt.verify(token, jwtSecret);
-      const user = await User.findById(decoded.userId);
-
-      if (!user) {
-        return res.status(404).json({ error: "User not found" });
-      }
-
-      res.json({ username: user.username });
-    } catch (error) {
-      res.status(401).json({ error: "Invalid token" });
-    }
-  });
-
   const token = jwt.sign({ userId: user._id }, jwtSecret, { expiresIn: "1h" });
   res.json({ token });
+});
+
+/**
+ * @openapi
+ * /api/user/{username}:
+ *   get:
+ *     summary: Get a user by username
+ *     parameters:
+ *       - in: path
+ *         name: username
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Username of the user to retrieve
+ *     responses:
+ *       200:
+ *         description: Successful retrieval of the user
+ *       404:
+ *         description: User not found
+ */
+app.get("/api/user/:username", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve the user" });
+  }
+});
+
+/**
+ * @openapi
+ * /api/users:
+ *   get:
+ *     summary: Get all users from the database
+ *     responses:
+ *       200:
+ *         description: Successful retrieval of all users
+ */
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to retrieve users from the database" });
+  }
+});
+
+/**
+ * @openapi
+ * /api/user/{username}:
+ *   delete:
+ *     summary: Delete a user by username
+ *     parameters:
+ *       - in: path
+ *         name: username
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Username of the user to delete
+ *     responses:
+ *       200:
+ *         description: User deleted successfully
+ *       404:
+ *         description: User not found
+ */
+app.delete("/api/user/:username", async (req, res) => {
+  try {
+    const user = await User.findOneAndDelete({ username: req.params.username });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to delete the user" });
+  }
 });
 
 app.listen(port, () => {
